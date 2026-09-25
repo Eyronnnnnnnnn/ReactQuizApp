d15a5cc2d06425f5aa57e3b2f14588a36f2b5d51
EYRON <aaronguillermo123456789@gmail.com>
2026-09-25 22:39:40 +0800

    updated

---
+++ Quiz-app/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+return(
+    <div className="bg-amber-300 w-full h-32 flex justify-center items-center ">
+        <h1 className="text-blue-300
+        ">Wellcome to my Quiz</h1>
+    </div>
+)
+}
